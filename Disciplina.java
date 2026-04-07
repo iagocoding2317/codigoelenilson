7db5cfa5e17fe7aad94a59eb460a2335e734739f
iagocoding2317 <115094044+iagocoding2317@users.noreply.github.com>
2026-04-07 09:54:43 -0300

    Update Disciplina.java

--- Disciplina.java
+++ Disciplina.java
@@ -1,32 +1,22 @@
+
 import java.util.ArrayList;
 
-public class Disciplina {
+public class Estudante {
     private String nome;
-    private String professor;
-    private int codigo;
-    private String turno;
-    ArrayList<Estudante> estudantes;
+    private String matricula;
+    private String curso;
+    private String periodo;
+    private ArrayList<Disciplina> disciplina;
 
-    public Disciplina(String nome, String professor, int codigo, String turno, Estudante estudantes) {
+    public Estudante(String nome, String matricula, String curso, String periodo,  ArrayList<Disciplina> disciplina) {
         this.nome = nome;
-        this.professor = professor;
-        this.codigo = codigo;
-        this.turno = turno;
-        this.estudantes = new ArrayList<>();
-    }
-
-    public void inserirEstudante(Estudante e) {
-        estudantes.add(e);
-    }
-
-    public void listarAlunos() {
-        System.out.println("Alunos da disciplina " + nome + ":");
-        for (Estudante e : estudantes) {
-            estudantes.exibir();
-        }
+        this.matricula = matricula;
+        this.curso = curso;
+        this.periodo = periodo;
+        this.disciplina = new ArrayList<>();
     }
 
     public void exibir() {
-        System.out.println("Disciplina: " + nome + " | Professor: " + professor + " | Código: " + codigo + " | Turno: " + turno + "Estudantes: " + estudantes);
+        System.out.println("Nome: " + nome + " Matrícula: " + matricula + " Curso: " + curso + "Período: " + periodo + "Disciplina: " + disciplina);
     }
 }
