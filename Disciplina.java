877646dc01533f40b92edb837ab84b6218eba93f
iagocoding2317 <115094044+iagocoding2317@users.noreply.github.com>
2026-04-14 09:14:01 -0300

    Add Disciplina class and refactor Estudante class

--- Disciplina.java
+++ Disciplina.java
@@ -1,22 +1,61 @@
-
 import java.util.ArrayList;
 
-public class Estudante {
+public class Disciplina {
     private String nome;
-    private String matricula;
+    private String professor;
     private String curso;
     private String periodo;
-    private ArrayList<Disciplina> disciplina;
+    private ArrayList<Estudante> estudantes;
 
-    public Estudante(String nome, String matricula, String curso, String periodo,  ArrayList<Disciplina> disciplina) {
+    public Disciplina(String nome, String professor, String curso, String periodo){
         this.nome = nome;
-        this.matricula = matricula;
+        this.professor = professor;
         this.curso = curso;
         this.periodo = periodo;
-        this.disciplina = new ArrayList<>();
+        this.estudantes = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public ArrayList<Estudante> getEstudantes() {
+        return estudantes;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public void setEstudantes(ArrayList<Estudante> estudantes) {
+        this.estudantes = estudantes;
     }
 
     public void exibir() {
-        System.out.println("Nome: " + nome + " Matrícula: " + matricula + " Curso: " + curso + "Período: " + periodo + "Disciplina: " + disciplina);
+        System.out.println("Nome: " + nome + " Professor: " + professor + " Curso: " + curso + " Período: " + periodo + "Estudantes: " + estudantes);
     }
 }
