33e72bdfb351c1706d1050a253b2b19f3479496b
iagocoding2317 <115094044+iagocoding2317@users.noreply.github.com>
2026-04-14 09:14:26 -0300

    Refactor Estudante class to include idade
    Added 'idade' field and corresponding getters/setters.

--- Estudante.java
+++ Estudante.java
@@ -3,17 +3,57 @@ public class Estudante {
     private String matricula;
     private String curso;
     private String periodo;
-    private Disciplina disciplina;
+    private int idade;
 
-    public Estudante(String nome, String matricula, String curso, String periodo, Disciplina disciplina) {
+    public Estudante(String nome, String matricula, String curso, String periodo, int idade) {
         this.nome = nome;
         this.matricula = matricula;
         this.curso = curso;
         this.periodo = periodo;
-        this.disciplina = disciplina;
+        this.idade = idade;
+    }
+    
+    public String getNome() {
+        return nome;
     }
 
-    public void exibir() {
-        System.out.println("Nome: " + nome + " Matrícula: " + matricula + " Curso: " + curso + "Período: " + periodo + "Disciplina: " + disciplina);
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String toString() {
+         return "Nome: " + nome + " Matrícula: " + matricula + " Curso: " + curso + " Período: " + periodo + " Idade: " + idade;
     }
 }
